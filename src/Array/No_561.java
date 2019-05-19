22f333dab6ac1deb06d2995309ae40a58a2b0eb8
潇湘夜雨 <sang879@sohu.com>
2019-05-20 00:20:34 +0800

    add (*master) leetcode no_561 数组拆分 I

---
+++ src/Array/No_561.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Arrays;
+
+public class No_561 {
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length - 1; i = i + 2) {
+            result += Math.min(nums[i], nums[i + 1]);
+        }
+        return result;
+    }
+}
